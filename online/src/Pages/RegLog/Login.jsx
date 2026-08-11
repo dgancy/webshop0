83ecc4786bd4d6741597374ca22afe5ff00d4a1b
dgancy <csabiger@gmail.com>
2026-08-11 19:19:34 +0200

    LogReqFrontToBack

--- online/src/Pages/RegLog/Login.jsx
+++ online/src/Pages/RegLog/Login.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 
 export default function Login() {
+  let [userName, SetUserName] = useState("");
+  let [passWord, SetPassword] = useState("");
+
+  function GatherData() {
+    const dataToBack = { userName, passWord };
+
+    console.log(dataToBack);
+  }
+
   return (
     <div>
       <p>Please Login with your account!</p>
-      <input type="text" placeholder="Username or Email address" />
-      <input type="password" placeholder="Password" />
+      <input
+        type="text"
+        id="username"
+        placeholder="Username"
+        value={userName}
+        onChange={(e) => SetUserName(e.target.value)}
+      />{" "}
+      <input
+        type="password"
+        id="password"
+        placeholder="Password"
+        value={passWord}
+        onChange={(e) => SetPassword(e.target.value)}
+      />
       <button>Login</button>
     </div>
   );
--- online/src/Pages/RegLog/Register.jsx
+++ online/src/Pages/RegLog/Register.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Register() {
+  let [userName, SetUserName] = useState("");
+  let [eMail, SetEmail] = useState("");
+  let [passWord, SetPassword] = useState("");
+
+  function GatherData() {
+    const dataToBack = { userName, eMail, passWord };
+
+    console.log(dataToBack);
+  }
+
   return (
     <div>
       <p>Please Register an account!</p>
-      <input type="text" placeholder="Username" />
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button onClick={}>Register</button>
+      <input
+        type="text"
+        id="username"
+        placeholder="Username"
+        value={userName}
+        onChange={(e) => SetUserName(e.target.value)}
+      />
+      <input
+        type="text"
+        id="email"
+        placeholder="Email"
+        value={eMail}
+        onChange={(e) => SetEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        id="password"
+        placeholder="Password"
+        value={passWord}
+        onChange={(e) => SetPassword(e.target.value)}
+      />
+      <button onClick={GatherData}>Register</button>
     </div>
   );
 }
