f50e1867cfda21344647a948f5f4c57b2b4a224b
dgancy <csabiger@gmail.com>
2026-08-27 19:49:09 +0200

    Order process part 2

--- online/src/Pages/Order/OrdersData.jsx
+++ online/src/Pages/Order/OrdersData.jsx
@@ -1,23 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function OrdersData() {
-    const nav = useNavigate();
+  const nav = useNavigate();
 
-const NavigateToFinalise = () =>{
+  const [userData, setUserData] = useState({
+    fullName: "",
+    mail: "",
+    phoneNumber: "",
+    location: "",
+  });
+
+  const NavigateToFinalise = async (e) => {
+    e.preventDefault();
+    console.log("UserData:", userData);
+
+    try {
+      const response = await fetch("http://localhost:5000/userdata", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      console.log(response);
+    } catch (error) {
+      console.log("Hiba", error);
+    }
+
     nav("/ordersummary");
-}
+  };
 
   return (
-    <form>
+    <form onSubmit={NavigateToFinalise}>
       <div>
-        <input placeholder="FullName" className="inputNormal" type="string"/>
-        <input placeholder="Email" className="inputNormal" type="string"/>
-        <input placeholder="Phone Number" className="inputNormal" type="string"/>
-        <input placeholder="Location" className="inputNormal" type="string"/>
+        <input
+          placeholder="FullName"
+          className="inputNormal"
+          type="string"
+          id="FullNameId"
+          value={userData.fullName}
+          onChange={(e) =>
+            setUserData({
+              ...userData,
+              fullName: e.target.value,
+            })
+          }
+        />
+        <input
+          placeholder="Email"
+          className="inputNormal"
+          type="string"
+          id="EmailId"
+          value={userData.mail}
+          onChange={(e) =>
+            setUserData({
+              ...userData,
+              mail: e.target.value,
+            })
+          }
+        />
+        <input
+          placeholder="Phone Number"
+          className="inputNormal"
+          type="string"
+          id="PhoneNumberId"
+          value={userData.phoneNumber}
+          onChange={(e) =>
+            setUserData({
+              ...userData,
+              phoneNumber: e.target.value,
+            })
+          }
+        />
+        <input
+          placeholder="Location"
+          className="inputNormal"
+          type="string"
+          id="LocationId"
+          value={userData.location}
+          onChange={(e) =>
+            setUserData({
+              ...userData,
+              location: e.target.value,
+            })
+          }
+        />
       </div>
       <div>
-        <button onClick={NavigateToFinalise}>Continue</button>
+        <button type="submit">Continue</button>
       </div>
     </form>
   );
