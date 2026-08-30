1c008571e5298e8a84dc40bb646a797bfedc6e61
dgancy <csabiger@gmail.com>
2026-08-30 20:06:24 +0200

    Order process part 4

--- backend/server.js
+++ backend/server.js
@@ -47,18 +47,30 @@ app.get("/shopcart", (req, res) => {
   res.json(shopCart);
 });
 
+let userData = [];
 
 app.post("/userdata", (req, res) => {
   //app.post where we want the data, request the data and response something for that.
-  const { fullName, mail, phoneNumber,location } = req.body;
+  const { fullName, mail, phoneNumber, location } = req.body;
   console.log("UserData from Frontend : ", req.body); //REQuest the bodz which contains the data from frontend
 
+  userData.push({
+    fullName: fullName,
+    mail: mail,
+    phoneNumber: phoneNumber,
+    location: location,
+  });
+  
   res.status(200).json({
     message: "ShopCart data received successfully : ", //backend write it for response.
     receivedData: req.body, //
   });
 });
 
+app.get("/userdata", (req, res) => {
+  console.log("UserData in backend : ", userData);
+  res.json(userData);
+});
 
 app.listen(5000, () => {
   console.log("Server fut: 5000");
--- online/src/Pages/Order/OrderSummary.jsx
+++ online/src/Pages/Order/OrderSummary.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import BooksData from "../../Images/Books/Books";
 
 export default function OrderSummary() {
+  const [shopCart, setShopCart] = useState([]);
+
+  const Books = BooksData;
+
+  const GetShopCart = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/shopcart");
+      const data = await response.json();
+
+      console.log("Backendtől kapott kosár:", data);
+
+      setShopCart(data);
+    } catch (error) {
+      console.error("Hiba a kosár lekérésekor:", error);
+    }
+  };
+
+  const [user, setUser] = useState([]);
+
+  const UserData = BooksData;
+
+  const GetUsderData = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/userdata");
+      const Udata = await response.json();
+
+      console.log("Backendtől kapott kosár:", Udata);
+
+      setUser(Udata);
+    } catch (error) {
+      console.error("Hiba a kosár lekérésekor:", error);
+    }
+  };
+
+  useEffect(() => {
+    GetShopCart();
+    GetUsderData();
+  }, []);
+
   return (
-    <div>
-      Final
-    </div>
-  )
+    <form>
+      <div>
+        <h1>Shopping Cart</h1>
+
+        {shopCart.map((item, index) => (
+          <div key={index}>
+            <img src={Books[item.bookId].image} alt={item.bookId} width="100" />
+            <h2>{item.bookId}</h2>
+            <p>Quantity: {item.quantity}</p>
+          </div>
+        ))}
+      </div>
+      <div></div>
+      <div>
+        <button>Continue</button>
+      </div>
+    </form>
+  );
 }
